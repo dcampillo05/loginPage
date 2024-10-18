c30658b1b5c0fdd85574a369249b7c21d5523714
Daniel Campillo Ricardi <dcampillo05@gmail.com>
2024-10-17 22:32:21 -0300

    feat: add forms login & signup

---
+++ src/app/singup/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import { SignUpForm } from "../../components/auth/signUpForm"
+
+export default function SignUp() {
+
+    return (
+        <SignUpForm />
+    )
+
+}
+
+
